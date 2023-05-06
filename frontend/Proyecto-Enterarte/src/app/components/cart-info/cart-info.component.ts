39c5066441b95ea278cb4026af0ce8c288f7eb00
villarrealtay <villarrealtay@gmail.com>
2023-05-05 21:08:04 -0300

    cart component

---
+++ frontend/Proyecto-Enterarte/src/app/components/cart-info/cart-info.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cart-info-event',
+  templateUrl: './cart-info.component.html',
+  styleUrls: ['./cart-info.component.css']
+})
+export class CreateEventComponent {
+
+}
