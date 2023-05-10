0204dce110797eadb572b6420f753d7261f0365e
JulianMeneses1 <julian.meneses11@gmail.com>
2023-05-10 13:21:40 -0300

    reestructuracion carpetas proyecto

--- frontend/Proyecto-Enterarte/src/app/app.module.ts
+++ frontend/Proyecto-Enterarte/src/app/app.module.ts
@@ -4,32 +4,34 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
-import { EventsComponent } from './components/events/events.component';
-import { EventItemComponent } from './components/event-item/event-item.component';
-import { CreateEventComponent } from './components/create-event/create-event.component';
-import { AboutComponent } from './components/about/about.component';
-import { ProfileUserComponent } from './components/profile-user/profile-user.component';
-import { HomeComponent } from './components/home/home.component';
-import { Error404Component } from './components/error404/error404.component';
-import { RegistroComponent } from './auth/registro/registro.component';
-import { LoginComponent } from './components/login/login.component';
+import { AboutModule } from './modules/about/about.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { CartModule } from './modules/cart/cart.module';
+import { CreateEventModule } from './modules/create-event/create-event.module';
+import { Error404Module } from './modules/error404/error404.module';
+import { EventsModule } from './modules/events/events.module';
+import { HomeModule } from './modules/home/home.module';
+import { ProfileUserModule } from './modules/profile-user/profile-user.module';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent,
-    EventsComponent,
-    EventItemComponent,
-    CreateEventComponent,
-    AboutComponent,
-    ProfileUserComponent,
-    LoginComponent,
-    HomeComponent,
-    Error404Component,
-    RegistroComponent,
+    FooterComponent   
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule, 
+    AppRoutingModule,
+    AboutModule,
+    AuthModule,
+    CartModule,
+    CreateEventModule,
+    Error404Module,
+    EventsModule,
+    HomeModule,
+    ProfileUserModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
