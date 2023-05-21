3865d047327eaa25331fc79e4132d26ae69fd4ba
Gerardo CastellanosTorres <92652145+GCastelP@users.noreply.github.com>
2023-05-21 18:55:49 -0300

    Add files via upload

---
+++ frontend/Proyecto-Enterarte/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import {Component } from '@angular/core';
+import { UserService } from './modules/profile-user/services/user.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Proyecto-Enterarte';
+  
+}
---
+++ frontend/Proyecto-Enterarte/src/app/modules/profile-user/pages/profile-user/profile-user.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-user',
+  templateUrl: './profile-user.component.html',
+  styleUrls: ['./profile-user.component.css']
+})
+export class ProfileUserComponent {
+
+}
+
