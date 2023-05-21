031c02f599c3bcbd459b83a8f38dbb805fcb6d89
sol2108 <ms.orrego21@gmail.com>
2023-05-21 20:52:14 -0300

    Cambios componentes

--- frontend/Proyecto-Enterarte/src/app/components/home/home.service.ts
+++ frontend/Proyecto-Enterarte/src/app/components/home/home.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HomeService {
+  private items: string[] = ['Item 1', 'Item 2', 'Item 3'];
 
-  constructor() { }
+  constructor() {}
+  getItems(): string[] {
+    // Retorna los items disponibles
+    return this.items;
+  }
+
+  addItem(item: string): void {
+    // Agrega un nuevo item a la lista
+    this.items.push(item);
+  }
+
+  removeItem(index: number): void {
+    // Remueve un item de la lista basado en su índice
+    if (index >= 0 && index < this.items.length) {
+      this.items.splice(index, 1);
+    }
+  }
 }
