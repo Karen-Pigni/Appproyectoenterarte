f299a5a8aab14462a9743be3b8fc2a0180e7241a
Karen Pigni <64334155+Karen-Pigni@users.noreply.github.com>
2023-06-20 04:38:19 -0300

    front y back integrado

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { StoreModule } from './modules/store/store.module';
+import { AdminModule } from './modules/admin/admin.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { ProfileModule } from './modules/profile/profile.module';
+import { HomeModule } from './modules/home/home.module';
+import { EventsModule } from './modules/events/events.module';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    SharedModule,
+    AuthModule,
+    HomeModule,
+    ProfileModule,
+    AdminModule,
+    StoreModule,
+    EventsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
