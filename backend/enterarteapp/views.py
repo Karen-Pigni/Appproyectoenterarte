663cb31dfa75f1bf30cf280c05835e8fa4a6b334
GCastelP <gcastelt@gmail.com>
2023-06-19 21:49:46 -0300

    Actualizacion backend

---
+++ backend/enterarteapp/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+# MBG
+from django.shortcuts import render
+from dj_rest_auth.registration.views import SocialLoginView
+from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
+from allauth.socialaccount.providers.oauth2.client import OAuth2Client
+
+
+class GoogleLogin(SocialLoginView):
+    adapter_class = GoogleOAuth2Adapter
+    callback_url = "http://localhost:4200/"
+    client_class = OAuth2Client
